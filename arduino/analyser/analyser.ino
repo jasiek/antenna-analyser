b97338fac7699edb137272d5c63c780ac71793b8
Jan Szumiec <jan.szumiec@gmail.com>
2015-07-01 22:45:08 +0100

    Draw a slight curve and a green dot wherever 0,0 is.

--- arduino/analyser/analyser.ino
+++ arduino/analyser/analyser.ino
@@ -8,6 +8,8 @@ void setup() {
   Serial.begin(57600);
   tft.begin();
   Serial.println("Width: " + String(tft.width()) + " Height: " + String(tft.height()));
+  tft.fillScreen(ILI9341_BLACK);
+  tft.drawPixel(0, 0, ILI9341_GREEN);
 }
 
 void loop() {
@@ -19,17 +21,17 @@ void drawSWRPlot(float begin, float end) {
   float f = begin; // kHz
   float ystep = tft.width() / 64;
 
-  for (int x = 0; x < tft.height(); x++) {
+  for (int x = tft.height(); x >= 0; --x) {
     float swr = measureSWR(f);
-    int y = tft.height() - swr * ystep;
-    tft.drawPixel(y, x, ILI9341_BLACK);
+    int y = tft.width() - swr * ystep;
+    tft.drawPixel(y, x, ILI9341_WHITE);
     
     f += xstep;
   }
 }
 
 float measureSWR(float f) {
-  return 32; 
+  return (f - 135) * (f - 135); 
 }
 
 
