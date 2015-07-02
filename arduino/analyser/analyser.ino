ca1caef8016bcc1656f4571aa8225f2e1605bd3d
Jan Szumiec <jan.szumiec@gmail.com>
2015-07-02 22:51:57 +0100

    Use hardware SPI instead (duh!).

--- arduino/analyser/analyser.ino
+++ arduino/analyser/analyser.ino
@@ -4,82 +4,46 @@
 
 #define BACKGROUND ILI9341_BLACK
 
-Adafruit_ILI9341 tft = Adafruit_ILI9341(8, 10, 11, 12, 9, 13);
+Adafruit_ILI9341 tft = Adafruit_ILI9341(10, 9, 8);
 
 void setup() {
   Serial.begin(57600);
   tft.begin();
   Serial.println("Width: " + String(tft.width()) + " Height: " + String(tft.height()));
   tft.setRotation(3);
+  tft.fillScreen(BACKGROUND);
 }
 
 void loop() {
   float swr;
   for (float f = 120; f < 30000; f += 137) {
-    swr = 127 / 100.0;
+    swr = 999 / 100.0;
     displayFrequency(f, swr);
   }
 }
 
-String previousFrequency;
-String previousSWR;
-
+bool labelsVisible = false;
 void displayFrequency(float f, float swr) {
-  int x, y;
-  
-  if (previousFrequency.length() == 0) {
+  tft.setTextSize(2);
+
+  if (!labelsVisible) {
     tft.setCursor(20, 20);
-    tft.setTextSize(2);
-    tft.setTextColor(ILI9341_RED);
+    tft.setTextColor(ILI9341_RED, BACKGROUND);
     tft.print("Frequency:");
-  }
-
-  tft.setCursor(150, 20);
-  x = tft.getCursorX();
-  y = tft.getCursorY();
-  tft.setTextColor(BACKGROUND);
-  tft.print(previousFrequency);
-  tft.setCursor(x, y);
-
-  tft.setTextColor(ILI9341_GREEN);
-  previousFrequency = String(f) + " MHz";
-  tft.print(previousFrequency);
-
-  if (previousSWR.length() == 0) {
+  
     tft.setCursor(20, 40);
-    tft.setTextColor(ILI9341_RED);
-    tft.print("VSWR:     ");
+    tft.setTextColor(ILI9341_RED, BACKGROUND);
+    tft.print("VSWR:");
+
+    labelsVisible = true;
   }
+  
+  tft.setCursor(150, 20);
+  tft.setTextColor(ILI9341_GREEN, BACKGROUND);
+  tft.print(String(f) + " kHz  ");
 
   tft.setCursor(150, 40);
-  x = tft.getCursorX();
-  y = tft.getCursorY();
-  tft.setTextColor(BACKGROUND);
-  tft.print(previousSWR);
-  tft.setCursor(x, y);
-  
-  tft.setTextColor(ILI9341_GREEN);
-  previousSWR = String(swr);
-  tft.print(previousSWR);
+  tft.setTextColor(ILI9341_GREEN, BACKGROUND);
+  tft.print(String(swr));
 }
-
-
-void drawSWRPlot(float begin, float end) {
-  float xstep = (end - begin) / tft.height();
-  float f = begin; // kHz
-  float ystep = tft.width() / 64;
-
-  for (int x = tft.height(); x >= 0; --x) {
-    float swr = measureSWR(f);
-    int y = tft.width() - swr * ystep;
-    tft.drawPixel(y, x, ILI9341_WHITE);
-    
-    f += xstep;
-  }
-}
-
-float measureSWR(float f) {
-  return (f - 135) * (f - 135); 
-}
-
 
