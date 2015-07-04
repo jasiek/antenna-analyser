632fbf7a92b7e69efac09488810f9d07046f5b90
Jan Szumiec <jan.szumiec@gmail.com>
2015-07-04 12:23:49 +0100

    Both screens are visible, SWR measuring routine is in.

--- arduino/analyser/analyser.ino
+++ arduino/analyser/analyser.ino
@@ -1,31 +1,51 @@
 #include "SPI.h"
 #include "Adafruit_GFX.h"
 #include "Adafruit_ILI9341.h"
+#include <AD9850.h>
 
 #define BACKGROUND ILI9341_BLACK
 
 Adafruit_ILI9341 tft = Adafruit_ILI9341(10, 9, 8);
+AD9850 ad9850(7, 6, 5);
+
+volatile byte mode = 0;
+volatile boolean blankScreen = true;
 
 void setup() {
+  // Set up PIN 3 for mode switching via microswitch.
+  pinMode(3, INPUT_PULLUP);
+  attachInterrupt(1, changeMode, FALLING);
+  
   Serial.begin(57600);
   tft.begin();
   Serial.println("Width: " + String(tft.width()) + " Height: " + String(tft.height()));
   tft.setRotation(3);
-  
 }
 
+float f = 123;
 void loop() {
-  displayPlot(130, 140, "2200m");
-  /* float swr;
-  for (float f = 120; f < 30000; f += 1) {
-    swr = 999 / 100.0;
-    displayFrequency(f, swr);
-  } */
+  float swr;
+
+  if (blankScreen) {
+    tft.fillScreen(BACKGROUND);
+    blankScreen = false;
+  }
+
+  if (mode == 0) {
+    displayFrequency(f += 21, swr);
+  } else {
+    displayPlot(130, 140, "2200m");
+  }
+}
+
+// Change screen. No debouncing, this is good enough for now.
+void changeMode() {
+  mode = (mode + 1) % 2;
+  blankScreen = true;
 }
 
 bool labelsVisible = false;
 void displayFrequency(float f, float swr) {
-  tft.fillScreen(BACKGROUND);
   tft.setTextSize(2);
 
   if (!labelsVisible) {
@@ -46,11 +66,10 @@ void displayFrequency(float f, float swr) {
 
   tft.setCursor(150, 40);
   tft.setTextColor(ILI9341_GREEN, BACKGROUND);
-  tft.print(String(swr));
+  tft.print(measureSWR(f));
 }
 
 void displayPlot(float begin, float end, String label) {
-  tft.fillScreen(BACKGROUND);
   tft.setTextSize(2);
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9341_RED);
@@ -68,6 +87,15 @@ void displayPlot(float begin, float end, String label) {
 }
 
 float measureSWR(float f) {
-  return (f - 137) * (f - 137);
-}
+  float fwd, rev;
+  ad9850.setfreq(f);
+  delay(10);
+  rev = analogRead(A0);
+  fwd = analogRead(A1);
 
+  if (rev >= fwd) {
+    return 999;
+  } else {
+    return (fwd + rev) / (fwd - rev);
+  }
+}
