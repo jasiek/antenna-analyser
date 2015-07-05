731d8ebe53c33c8b63b6466ccaafd7c2d402424f
Jan Szumiec <jan.szumiec@gmail.com>
2015-07-05 22:01:49 +0100

    Updated code to display a plot of SWR versus frequency, and tested on my local antenna.

--- arduino/analyser/analyser.ino
+++ arduino/analyser/analyser.ino
@@ -8,13 +8,23 @@
 Adafruit_ILI9341 tft = Adafruit_ILI9341(10, 9, 8);
 AD9850 ad9850(7, 6, 5);
 
-volatile byte mode = 0;
+const String nameOfBand[] =  { "ALL", "2200m", "160m", "80m", "40m", "30m", "20m",    "17m", "15m",   "12m", "10m"};
+const double startOfBand[] = { 135E3, 135.7E3, 1.81E6, 3.5E6,  7E6, 10.1E6,  14E6, 18.068E6,  21E6, 24.89E6, 28E6};
+const double endOfBand[] =   { 29.7E6, 137.8E3,    2E6, 3.8E6,7.2E6, 10.15E6, 14.35E6, 18.168E6, 21.450E6, 24.99E6, 29.7E6};
+const double steps[] =       { 1E6,    100,    3E3,   3E3,  3E3,    50,   3E3,     3E3,     3E3,    3E3,   6E3};
+
+volatile byte mode = 1;
 volatile boolean blankScreen = true;
+volatile byte band = 0; 
+volatile double f = startOfBand[band];
 
 void setup() {
   // Set up PIN 3 for mode switching via microswitch.
+  pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
-  attachInterrupt(1, changeMode, FALLING);
+  pinMode(4, INPUT_PULLUP);
+  attachInterrupt(0, changeMode, FALLING);
+  attachInterrupt(1, changeFrequency, RISING);
   
   Serial.begin(57600);
   tft.begin();
@@ -22,7 +32,6 @@ void setup() {
   tft.setRotation(3);
 }
 
-float f = 123;
 void loop() {
   float swr;
 
@@ -32,20 +41,42 @@ void loop() {
   }
 
   if (mode == 0) {
-    displayFrequency(f += 21, swr);
+    displayFrequency(f, swr);
   } else {
-    displayPlot(130, 140, "2200m");
+    Serial.println("BAND: " + String(band) + ", " + nameOfBand[band]);
+    displayPlot(startOfBand[band], endOfBand[band], nameOfBand[band]);
   }
 }
 
 // Change screen. No debouncing, this is good enough for now.
 void changeMode() {
-  mode = (mode + 1) % 2;
-  blankScreen = true;
+  band = (band + 1) % 11;
+  
 }
 
+void changeFrequency() {
+  switch(digitalRead(4)) {
+    case LOW:
+    f += 10E3;
+    break;
+    case HIGH:
+    f -= 10E3;
+  }
+
+  if (f < 136E3) {
+    f = 28E6;
+    return;
+  }
+
+  if (f > 28E6) {
+    f = 136E3;
+    return;
+  }
+}
+
+
 bool labelsVisible = false;
-void displayFrequency(float f, float swr) {
+void displayFrequency(double f, double swr) {
   tft.setTextSize(2);
 
   if (!labelsVisible) {
@@ -69,20 +100,45 @@ void displayFrequency(float f, float swr) {
   tft.print(measureSWR(f));
 }
 
-void displayPlot(float begin, float end, String label) {
+void displayPlot(double begin, double end, String label) {
+  tft.fillScreen(BACKGROUND);
   tft.setTextSize(2);
   tft.setCursor(0, 0);
   tft.setTextColor(ILI9341_RED);
   tft.println(label);
   
-  float xstep = (end - begin) / tft.width();
-  float ystep = tft.height() / 10;
+  double xstep = (end - begin) / tft.width();
+  double ystep = tft.height() / 10;
 
+  float maxSWR = 0;
+  float minSWR = 1000;
+
+  Serial.println("start= " + String(begin));
+  Serial.println("end  = " + String(end));
+  Serial.println("xstep= " + String(xstep));
+  Serial.println("ystep= " + String(ystep));
+
+  int prevx = 0;
+  int prevy = 240;
   for (int x = 0; x < tft.width(); x++) {
-    float f = begin + x * xstep;
+    double f = begin + x * xstep;
+    float swr = measureSWR(f);
 
-    int y = tft.height() - measureSWR(f) * ystep;
-    tft.drawPixel(x, y, ILI9341_GREEN); 
+    if (swr < minSWR) { minSWR = swr; }
+    if (swr > maxSWR) { maxSWR = swr; }
+    
+    int y = tft.height() - swr * ystep;
+    tft.drawLine(prevx, prevy, x, y, ILI9341_GREEN);
+    prevx = x;
+    prevy = y;
+
+    if (x % 30 == 0) {
+      tft.setTextSize(1);
+      tft.setCursor(0, 50);
+      tft.setTextColor(ILI9341_BLUE, BACKGROUND);
+      tft.println("min= " + String(minSWR));
+      tft.println("max= " + String(maxSWR));
+    }
   }
 }
 
