207c77b5fef2b35100edd2ab0485c26e2da1e69a
Jan Szumiec <jan.szumiec@gmail.com>
2015-08-04 22:53:25 +0100

    All menus work flawlessly.

--- arduino/analyser/analyser.ino
+++ arduino/analyser/analyser.ino
@@ -9,13 +9,19 @@
 Adafruit_ILI9341 tft = Adafruit_ILI9341(10, A3, A2);
 AD9850 ad9850(7, 6, 5);
 
-const String nameOfBand[] =  { "ALL",     "2200m",    "160m",     "80m",    "40m",    "30m",    "20m",    "17m",    "15m",      "12m",    "10m"};
-const double startOfBand[] = { 135E3,     135.7E3,    1.81E6,     3.5E6,    7E6,      10.1E6,   14E6,     18.068E6,  21E6,      24.89E6,  28E6};
-const double endOfBand[] =   { 29.7E6,    137.8E3,    2E6,        3.8E6,    7.2E6,    10.15E6,  14.35E6,  18.168E6,  21.450E6,  24.99E6,  29.7E6};
-const double steps[] =       { 1E6,       100,        3E3,        3E3,      3E3,      300,      3E3,      3E3,       3E3,       3E3,      6E3};
+const String nameOfBand[] =  { "ALL",     "2200m",    "160m",     "80m",    "40m",    "30m",    "20m",    "17m",    "15m",      "12m",    "10m"   };
+const double startOfBand[] = { 135E3,     135.7E3,    1.81E6,     3.5E6,    7E6,      10.1E6,   14E6,     18.068E6,  21E6,      24.89E6,  28E6    };
+const double endOfBand[] =   { 29.7E6,    137.8E3,    2E6,        3.8E6,    7.2E6,    10.15E6,  14.35E6,  18.168E6,  21.450E6,  24.99E6,  29.7E6  };
+const double steps[] =       { 1E6,       100,        3E3,        3E3,      3E3,      300,      3E3,      3E3,       3E3,       3E3,      6E3     };
 const int numBands = 11;
 
-volatile boolean blankScreen = true;
+// What kind of actions can be performed using the rotary encoder?
+typedef enum {
+  NONE,
+  CLICK,
+  LEFT,
+  RIGHT
+} Action;
 
 void setup() {
   pinMode(2, INPUT_PULLUP); // Rotary encoder CLK
@@ -29,46 +35,34 @@ void setup() {
 }
 
 void loop() {
-  float swr;
-
-  if (blankScreen) {
-    tft.fillScreen(BACKGROUND);
-    blankScreen = false;
-  }
-
-  userInterfaceLoop();
-}
-
-void userInterfaceLoop() {
   byte nextUIState = 9;
+
   while(1) {
+    tft.fillScreen(BACKGROUND);
+ 
     switch(nextUIState) {
       case 9:
         nextUIState = mainMenu();
         break;
       case 0:
-        tft.fillScreen(ILI9341_RED);
+        displayAllBands();
         nextUIState = 9;
         break;
       case 1:
-        tft.fillScreen(ILI9341_GREEN);
+        displaySingleBand(chooseBandMenu());
         nextUIState = 9;
         break;
       case 2:
-        tft.fillScreen(ILI9341_BLUE);
+        displaySummary();
         nextUIState = 9;
         break;
     }
   }
 }
 
-typedef enum {
-  NONE,
-  CLICK,
-  LEFT,
-  RIGHT
-} Action;
-
+// Display the main menu and handle view changes resulting from the operation of the
+// rotary encoder.
+// Returns to calling function when this menu is dismissed, communicating the menu state up the chain.
 byte mainMenu() {
   byte mainMenuState = 0;
   tft.setTextSize(1);
@@ -76,9 +70,9 @@ byte mainMenu() {
   tft.setCursor(10, 0);
   tft.print("Plot all bands");
   tft.setCursor(10, 10);
-  tft.println("Plot single band");
+  tft.print("Plot single band");
   tft.setCursor(10, 20);
-  tft.println("Display summary");
+  tft.print("Display summary");
 
   while (1) {
     if (mainMenuState == 0) { tft.setTextColor(ILI9341_YELLOW, BACKGROUND); } else { tft.setTextColor(BACKGROUND, BACKGROUND); }
@@ -104,30 +98,74 @@ byte mainMenu() {
   }
 }
 
+byte chooseBandMenu() {
+  byte menuState = 0;
+  byte i;
+  tft.setTextColor(ILI9341_RED, BACKGROUND);
+  for ( i = 0; i < numBands; i++) {
+    tft.setCursor(10, i * 10);
+    tft.print(nameOfBand[i]);
+  }
+
+  while (1) {
+    for ( i = 0; i < numBands; i++) {
+      tft.setCursor(0, i * 10);
+      if (menuState == i) { tft.setTextColor(ILI9341_YELLOW, BACKGROUND); } else { tft.setTextColor(BACKGROUND, BACKGROUND); }
+      tft.print(">");
+    }
+
+    switch (awaitAction()) {
+      case CLICK:
+        return menuState;
+      case LEFT:
+        menuState = (menuState - 1) % numBands;
+        break;
+      case RIGHT:
+        menuState = (menuState + 1) % numBands;
+        break;
+    }
+  }
+}
+
+// Await user interaction by setting interrupt handlers and looping on a variable modified from
+// within an ISR.
 volatile Action selectedAction;
 byte awaitAction() {
   selectedAction = NONE;
   
   detachInterrupt(0);
   detachInterrupt(1);
-  attachInterrupt(0, mainMenuClick, CHANGE);
-  attachInterrupt(1, mainMenuMove, FALLING);
+  attachInterrupt(0, clickHandler, CHANGE);
+  attachInterrupt(1, rollHandler, FALLING);
   interrupts();
   
   while (selectedAction == NONE);
   return selectedAction;
 }
 
-void mainMenuMove() {
+// Handles left- and right-turns on the rotary encoder
+void rollHandler() {
   noInterrupts();
 
   selectedAction = (digitalRead(2) == digitalRead(4)) ? LEFT : RIGHT;
 }
 
-void mainMenuClick() {
+// Handles clicks on the rotary encoder
+void clickHandler() {
   noInterrupts();
 
   selectedAction = CLICK;
+}
+
+
+void displayAllBands() {
+  displayPlot(135E5, 29E6, "ALL");
+  awaitAction();
+}
+
+void displaySingleBand(byte band) {
+  displayPlot(startOfBand[band], endOfBand[band], nameOfBand[band]);
+  awaitAction();
 }
 
 void displaySummary() {
@@ -150,6 +188,8 @@ void displaySummary() {
     tft.setCursor(18 * 6, i * 8);
     tft.print(swr);
   }
+
+  awaitAction();
 }
 
 bool labelsVisible = false;
